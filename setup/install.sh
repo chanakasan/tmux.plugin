58f99862d991406087073ee4ed5690cfb6a5193e
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-01-18 08:44:21 +0530

    rev

--- setup/install.sh
+++ setup/install.sh
@@ -1,12 +1,46 @@
+#!/bin/bash
+
+set -e
+
 main() {
-  ln -nfs ~/nex-tmux/src/main.conf ~/.tmux.conf
+  local start_text='__nex_tmux_start'
+  local end_text='__nex_tmux_end'
+  local bashrc="$HOME/.bashrc"
+  local nex_tmux_path=$(get_root_path)/nex-tmux
+  echo " Installing - Nex Tmux"
+  remove_from_bashrc
+  copy_to_bashrc
+  create_symlinks
+  echo ""
+  echo " done"
+  echo ""
 }
 
-write_config() {
-#__nex_tmux_start
-export nex_tmux_path=$HOME/nex-tmux
-export PATH=$nex_tmux_path/bin:$PATH
-#__nex_tmux_end
+create_symlinks() {
+  ln -nfs $nex_tmux_path/src/main.conf $HOME/.tmux.conf
 }
 
+get_root_path() {
+  local user=$(whoami)
+  if [ "$user" = "codespace" ]; then
+    echo /workspaces/.codespaces/.persistedshare
+  else
+    echo $HOME/dotfiles
+  fi
+}
+
+remove_from_bashrc() {
+  sed -i '/#'$start_text'/,/#'$end_text'/{d}' $bashrc
+}
+
+copy_to_bashrc() {
+  echo "" >> $bashrc
+  echo "#$start_text" >> $bashrc
+  echo 'export nex_tmux_path='$nex_tmux_path >> $bashrc
+  echo 'export PATH=$nex_tmux_path/bin:$PATH' >> $bashrc
+  echo "#$end_text" >> $bashrc
+  echo "" >> $bashrc
+}
+
+# _end_
 main
