77d9f584a64ec19cd4cbd4c7562e4fb8cadbdeb5
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-05-25 13:50:57 +0530

    rev

--- setup/install.sh
+++ setup/install.sh
@@ -2,4 +2,4 @@ nx_tmux_root=$(nex _plugin "tmux")
 
 echo " Install - tmux.plugin"
 ln -nfs $nx_tmux_root/.internal/main.conf $HOME/.tmux.conf
-echo " => created .tmux.conf symlink"
+echo " => symlinked .tmux.conf"
