fa20f980d3c515c03ad49e58a32ce7bfa45245dc
Chanaka Abeysinghe <chanakasan@gmail.com>
2025-03-10 21:36:37 +0530

    rev

--- setup/install.sh
+++ setup/install.sh
@@ -1,10 +1,5 @@
-create_symlinks() {
-  local root=$(nex _plugin "tmux")
-  echo " Install - tmux.plugin"
-  ln -nfs $root/.internal/main.conf $HOME/.tmux.conf
-  echo " => symlinked .tmux.conf"
-  echo " done"
-  echo
-}
-
-create_symlinks
+root=$(nex _plugin "tmux")
+echo Symlinking tmux conf
+ln -nfs $root/userland/main.conf $HOME/.tmux.conf
+echo "=> symlinked .tmux.conf"
+echo done
